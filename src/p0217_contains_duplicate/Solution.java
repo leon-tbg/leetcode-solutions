e3b0dbafa4280ad79ba2dfacff5c659b95a2c768
leon-tbg <leon.thomasberger@gmail.com>
2026-09-14 21:21:33 +0200

    solve p0242

--- src/p0217_contains_duplicate/Solution.java
+++ src/p0217_contains_duplicate/Solution.java
@@ -2,7 +2,7 @@ package p0217_contains_duplicate;
 
 import java.util.HashSet;
 
-public class Solution {
+class Solution {
     // Time: O(n), Space: O(n)
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
