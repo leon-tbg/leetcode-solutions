0e59480afa8686a377a34c3c3cd31fcd6b7de243
leon-tbg <leon.thomasberger@gmail.com>
2026-09-14 23:46:52 +0200

    solve p0347

---
+++ src/p0347_top_k_frequent_elements/Solution.java
@@ -0,0 +1,34 @@
+package p0347_top_k_frequent_elements;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+class Solution {
+        // Time: O(n + d * log k), Space: O(d) | d = number of distinct numbers
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        int[] res = new int[k];
+
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int[] tmp = {entry.getKey(), entry.getValue()};
+
+            minHeap.add(tmp);
+            if(minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        for (int i = 0; i < k; i++) {
+            res[i] = minHeap.poll()[0];
+        }
+
+        return res;
+    }
+}
