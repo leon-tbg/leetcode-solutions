8be683293f6a4346776a09de6f83a6d1f6b17b82
leon-tbg <leon.thomasberger@gmail.com>
2026-09-14 22:50:01 +0200

    solve p0049

---
+++ src/p0049_group_anagrams/Solution.java
@@ -0,0 +1,23 @@
+package p0049_group_anagrams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+    // Time: O(n * k * log k), Space: O(n * k) | n = number of strings, k = max length of a string
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sortedString = new String(chars);
+
+            map.computeIfAbsent(sortedString, v -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
