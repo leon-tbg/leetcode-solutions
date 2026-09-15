e305ebb2f0bcda086d5255c24fd307c5160b943f
leon-tbg <leon.thomasberger@gmail.com>
2026-09-15 20:59:42 +0200

    solve p0155

---
+++ src/p0155_min_stack/MinStack.java
@@ -0,0 +1,50 @@
+package p0155_min_stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class MinStack {
+    // Runtime: O(1), Memory: O(1) | for all methods
+
+    private Deque<Integer> stack;
+    private Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int value) {
+        stack.push(value);
+
+        if (value <= minStack.peek()) {
+            minStack.push(value);
+        }
+    }
+
+    public void pop() {
+        int value = stack.pop();
+
+        if (value == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(value);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
