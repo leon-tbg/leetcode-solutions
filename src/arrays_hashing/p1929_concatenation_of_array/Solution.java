8a5546b34e6290cac8dd203f00d98e4741c1afd0
leon-tbg <leon.thomasberger@gmail.com>
2026-09-16 00:03:18 +0200

    solve p1929

--- src/arrays_hashing/p1929_concatenation_of_array/Solution.java
+++ src/arrays_hashing/p1929_concatenation_of_array/Solution.java
@@ -1,4 +1,16 @@
 package arrays_hashing.p1929_concatenation_of_array;
 
-public class Solution {
+// Runtime: O(n), Memory: O(n)
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            ans[i] = nums[i];
+            ans[i + nums.length] = nums[i];
+        }
+
+        return ans;
+    }
 }
