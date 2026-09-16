bb12dd256b0837e5630b833d3a4feaa063ddd435
leon-tbg <leon.thomasberger@gmail.com>
2026-09-16 21:09:07 +0200

    solve p0706

---
+++ src/arrays_hashing/p0706_design_hashmap/MyHashMap.java
@@ -0,0 +1,66 @@
+package arrays_hashing.p0706_design_hashmap;
+
+import java.util.LinkedList;
+
+// Runtime: Best: O(1) Worst: O(n), Memory: O(size + n)
+class MyHashMap {
+    class Entry {
+        int key, value;
+
+        Entry(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private LinkedList<Entry>[] buckets;
+    private final int size = 1000;
+
+    public MyHashMap() {
+        buckets = new LinkedList[size];
+    }
+
+    private int hash(int key) {
+        return key % size;
+    }
+
+    public void put(int key, int value) {
+        int idx = hash(key);
+
+        if (buckets[idx] == null) buckets[idx] = new LinkedList<>();
+
+        for (Entry e : buckets[idx]) {
+            if (e.key == key) {
+                e.value = value;
+                return;
+            }
+        }
+
+        buckets[idx].add(new Entry(key, value));
+    }
+
+    public int get(int key) {
+        int idx = hash(key);
+
+        if (buckets[idx] == null) return -1;
+
+        for (Entry e : buckets[idx]) {
+            if (e.key == key) return e.value;
+        }
+
+        return -1;
+    }
+
+    public void remove(int key) {
+        int idx = hash(key);
+
+        if (buckets[idx] == null) return;
+
+        for (Entry e : buckets[idx]) {
+            if (e.key == key) {
+                buckets[idx].remove(e);
+                return;
+            }
+        }
+    }
+}
