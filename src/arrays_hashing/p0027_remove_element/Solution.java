e89869e82749ceae3573711fa75643537313d466
leon-tbg <leon.thomasberger@gmail.com>
2026-09-16 19:20:17 +0200

    solve p0027

---
+++ src/arrays_hashing/p0027_remove_element/Solution.java
@@ -0,0 +1,16 @@
+package arrays_hashing.p0027_remove_element;
+
+// Runtime: O(n), Memory: O(1)
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k++] = nums[i];
+            }
+        }
+
+        return k;
+    }
+}
